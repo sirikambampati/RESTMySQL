92ec406fcf75a6c5d7c08892e08fef1484fab164
sirikambampati <sirikambampati21@gmail.com>
2020-02-07 11:39:59 -0500

    y

--- src/main/java/com/example/book/BookApplication.java
+++ src/main/java/com/example/book/BookApplication.java
@@ -8,5 +8,6 @@ public class BookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
+        System.out.println("Checking Git");
     }
 }
